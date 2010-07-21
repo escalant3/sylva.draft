99a8a087ff867fc0b1c0f5fcb9a4ea705e589503
Diego Muñoz Escalante <escalant3@gmail.com>
2010-07-21 15:42:44 +0200

    Deleted comments from urls.py

--- urls.py
+++ urls.py
@@ -3,22 +3,15 @@ import os
 from django.conf.urls.defaults import patterns, include
 from django.contrib.auth.views import login, logout
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 site_media = os.path.join(os.path.dirname(__file__), 'site_media')
 
 urlpatterns = patterns('',
-    # Example:
-    (r'^', include('graph.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': site_media}),
-    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
     (r'^accounts/login/$', login),
     (r'^accounts/logout/$', logout, {'next_page': '/'}),
+    (r'^', include('graph.urls')),
 )
