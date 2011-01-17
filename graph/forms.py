4b5f26eeb631d18b8aaf33a3dbc5cfb372ca7675
Diego Muñoz Escalante <escalant3@gmail.com>
2011-01-17 15:52:37 -0500

    Removed remaining print statement

--- graph/forms.py
+++ graph/forms.py
@@ -4,6 +4,5 @@ from graph.models import Neo4jGraph
 GRAPHS = [(s.id, s.name) for s in Neo4jGraph.objects.all()]
 
 class UploadCSVForm(forms.Form):
-    print GRAPHS
     graph = forms.ChoiceField(choices=GRAPHS)
     csv_file = forms.FileField()
