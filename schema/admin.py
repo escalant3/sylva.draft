81e2d2c4abba9e4010f20963347d377d87f902ce
Diego Muñoz Escalante <escalant3@gmail.com>
2011-03-30 12:04:47 -0400

    Fixed default properties admin menus

--- schema/admin.py
+++ schema/admin.py
@@ -5,13 +5,19 @@ from django.contrib import admin
 class NodeDefaultPropertyInline(admin.TabularInline):
     model = NodeDefaultProperty
 
+class NodeTypeAdmin(admin.ModelAdmin):
+    inlines = [NodeDefaultPropertyInline]
+
 class EdgeDefaultPropertyInline(admin.TabularInline):
     model = EdgeDefaultProperty
+
+class EdgeTypeAdmin(admin.ModelAdmin):
+    inlines = [EdgeDefaultPropertyInline]
 
 class ValidRelationAdmin(admin.ModelAdmin):
     list_filter = ['schema']
 
 admin.site.register(Schema)
-admin.site.register(NodeType)
-admin.site.register(EdgeType)
+admin.site.register(NodeType, NodeTypeAdmin)
+admin.site.register(EdgeType, EdgeTypeAdmin)
 admin.site.register(ValidRelation, ValidRelationAdmin)
